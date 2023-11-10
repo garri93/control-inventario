8a3c380db195f6dbd363f1abe87f107359c03f20
carlos <carlos@goodrebels.loc>
2023-11-10 18:00:05 +0100

    se crea la incriptacion y falta añadir correo a formularios

--- models/User.php
+++ models/User.php
@@ -194,6 +194,7 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
         if (parent::beforeSave($insert)) {
             if ($this->isNewRecord) {
                 $this->auth_key = \Yii::$app->security->generateRandomString();
+                $this->password = Yii::$app->getSecurity()->generatePasswordHash($this->password);
             }
             return true;
         }
@@ -226,16 +227,21 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
    
 
 
-
+    
     /**
      * Validates password
      *
      * @param string $password password to validate
      * @return bool if password provided is valid for current user
-     */
+  *//*
     public function validatePassword($password)
     {
         return $this->password === $password;
+    }*/
+
+    public function validatePassword($password) 
+    {
+        return (Yii::$app->getSecurity()->validatePassword($password, $this->password)); 
     }
 
   /**
