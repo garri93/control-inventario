7cea055cd0b972e275191257c394e1d0dfcc1f4b
Carlos <carlosgg.trabajo@gmail.com>
2023-11-17 00:45:57 +0100

    Se implemta sistema de tab, se colocan bordes y se ocultan parte de los filtros de la gridview de oficinas

--- views/customer/view.php
+++ views/customer/view.php
@@ -23,65 +23,83 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 <section>
-<div class="customer-view">
-<div class="row">
-    <div class="col-sm-6 col-md-6 col-xl-6">
+    <div class="customer-view">
+        <div class="row">
+        <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+        <div class="row border">
+            <div class="col-sm-6 col-md-6 col-xl-6 border-right">
 
-    
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?php if(Yii::$app->user->identity->isUserAdmin()):?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?><?php endif; ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'internal_code',
-            'name',
-            'cif',
-            
-            
-
-            
-        ],
-    ]) ?>
-    </div>
-
-    <div class="col-sm-6 col-md-6 col-xl-6">
-
-    <h1>Oficinas</h1>
-
-    <?= GridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $searchModel,
-                    //'showHeader'=> false,
-                    'columns' => [
-                        [
-                            'attribute' => 'name',   
-                            'format' => 'raw',
-                            'value'=> function ($model) {
-                                         //return Url::to([$dataProvider['name'],'/office/view', 'id' => $dataProvider['id']]);
-                                         //return Html::a(Html::encode($dataProvider['name']),'/office/view','id' => $dataProvider['id']);
-                                         return Html::a($model->name, ['office/view', 'id' => $model->id]);
-                                     },
-                            ],
-
+                
+                
+                <p>
+                    <?= Html::a('Actualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+                    <?php if(Yii::$app->user->identity->isUserAdmin()):?>
+                    <?= Html::a('Borrar', ['delete', 'id' => $model->id], [
                         
+                        'class' => 'btn btn-danger',
+                        'data' => [
+                            'confirm' => 'Are you sure you want to delete this item?',
+                            'method' => 'post',
+                        ],
+                    ]) ?><?php endif; ?>
+                </p>
+
+                <?= DetailView::widget([
+                    'model' => $model,
+                    'attributes' => [
+                        'internal_code',
+                        'name',
+                        'cif',                     
                     ],
-                ]); ?>
+                ]) ?>
+            </div>
+
+            <div class="col-sm-6 col-md-6 col-xl-6">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Oficinas</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                    </li> -->
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        
+                    <?= GridView::widget([
+                                'dataProvider' => $dataProvider,
+                                //'showHeader'=> false, //Ocultar todo el header paginacion, label, cuadro de busqueda.
+                                'filterModel' => $searchModel, //Mostrar cuadro de busqueda.
+                                'summary' => '', //Ocultar texto paginacion superior
+                                'before' => false,
+                                
+                                'columns' => [
+                                    [
+                                        
+                                        'enableSorting' => false, // Desactivar la clasificacion ascendente y descendente
+                                        'label' => false, //Ocultar fila de label
+                                        'attribute' => 'name',   
+                                        'format' => 'raw',
+                                        'value'=> function ($model) {
+                                                                return Html::a($model->name, ['office/view', 'id' => $model->id]);
+                                                    },
+                                    ],                
+                                ],
+                        ]); ?>
+                    </div>
+
+                    <!-- <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <p>hola</p> 
+                    </div>
+
+                    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div> -->
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-</div>
 
 </section>
